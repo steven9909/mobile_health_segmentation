e45525acfc5fd05d3d0e2b759a99f3ca301c7ff9
steven9909 <sshyun@edu.uwaterloo.ca>
2023-10-22 14:38:15 -0400

    add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+
+frameWidth = 256
+frameHeight = 256
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
