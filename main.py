2e06fa022ecd22a07f1b59d2ef4e432728479eff
steven9909 <sshyun@edu.uwaterloo.ca>
2023-11-14 19:04:50 -0500

    add multithreading processes

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import cv2
+from workers import DelegationWorker
+import multiprocessing as mp
+from pathlib import Path
 
-frameWidth = 256
-frameHeight = 256
-cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10, 150)
+if __name__ == "__main__":
+    frame_width = 1440
+    frame_height = 960
 
-while True:
-    success, img = cap.read()
-    cv2.imshow("Result", img)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+    save_dir = Path("./saved/")
+
+    patch_size = 256
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3, frame_width)
+    cap.set(4, frame_height)
+
+    process_event = mp.Event()
+    done_event = mp.Event()
+
+    delegation = DelegationWorker(process_event, done_event)
+
+    while True:
+        process_event.set()  # we are ready to pass off the image to the Delegation worker
+        done_event.wait()  # wait for the delegation worker to finish
+        done_event.clear()  # clear the flag
